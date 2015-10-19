046151606c097207a0dcf8daa655520582edde06
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-10-19 19:24:08 +0300

    add simple app implementation

--- src/main/java/vmelnychuk/spring/App.java
+++ src/main/java/vmelnychuk/spring/App.java
@@ -1,13 +1,16 @@
 package vmelnychuk.spring;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private Client client;
+    private ConsoleEventLogger eventLogger;
+    public static void main(String[] args) {
+        App app = new App();
+        app.client = new Client("1", "John Smith");
+        app.eventLogger = new ConsoleEventLogger();
+        app.logEvent("Some event from user 1");
+    }
+    public void logEvent(String message) {
+        String msg = message.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(msg);
     }
 }
---
+++ src/main/java/vmelnychuk/spring/ConsoleEventLogger.java
@@ -0,0 +1,7 @@
+package vmelnychuk.spring;
+
+public class ConsoleEventLogger {
+    public void logEvent(String message) {
+        System.out.println(message);
+    }
+}
