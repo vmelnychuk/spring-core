f1a842bdde0cbe5591bee91eede82e5c9def8be6
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-10-19 19:02:48 +0300

    initial commit

---
+++ src/main/java/vmelnychuk/spring/App.java
@@ -0,0 +1,13 @@
+package vmelnychuk.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
